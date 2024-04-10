6c6586c06d07c604bb2b3b30ad208a8d09ca94b4
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2024-04-10 16:49:39 +0500

    added prefabs

--- core/Assets/Scripts/MiniGames/MidCore/Botstrap.cs
+++ core/Assets/Scripts/MiniGames/MidCore/Botstrap.cs
@@ -6,10 +6,12 @@ public class Botstrap : MonoBehaviour
 {
     [SerializeField] LevelHolder _levelHolder;
     [SerializeField] MinigamesController _minigamesController;
+    [SerializeField] LevelGenerator _levelGenerator;
 
     void Start()
     {
         _minigamesController.Init();
         _levelHolder.Init();
+        _levelGenerator.Init(); 
     }
 }
