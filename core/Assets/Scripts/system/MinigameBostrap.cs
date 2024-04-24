d749c2c31fc16fa1fc71bffc8868bcecc319de49
Katya Zavyalova <82714610+KatyaZav@users.noreply.github.com>
2024-04-24 16:55:52 +0500

    rebalance

--- core/Assets/Scripts/system/MinigameBostrap.cs
+++ core/Assets/Scripts/system/MinigameBostrap.cs
@@ -33,7 +33,7 @@ namespace ColorChooseGame
             _gameLogic.Inizialize(rnd);
             _points.Inizialize(0);
 
-            _slider.Inizialize(15*PlayerController.GetHealth());
+            _slider.Inizialize(10*PlayerController.GetHealth());
         }
 
         private void OnDisable()
